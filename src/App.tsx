32fed57c438f533c9d438eb96ece0864c105bf55
Michael <61318098+mikelbts89@users.noreply.github.com>
2022-02-06 22:34:20 +0200

    added hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom'
 import Routing from './Components/Routing';
 import ResponsiveAppBar from './Components/NavBar/NavBar';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <header>
-          <ResponsiveAppBar/>  
+          <ResponsiveAppBar />
         </header>
         <main>
           <Routing />
         </main>
         <footer></footer>
       </div>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
