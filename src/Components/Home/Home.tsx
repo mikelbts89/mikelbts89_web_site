3708b99056f877bc7825e823e2e80a308d14c78f
Michael <61318098+mikelbts89@users.noreply.github.com>
2022-02-06 21:39:13 +0200

    added comands

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 import "./Home.css"
 
 function Home() {
-  return <div >
-    <Container fixed maxWidth="xl">
+  return <div className='home' >
+    {/* <img className='my_img' src="/5336.jpg" alt="" /> */}
+    <div className="text_about">
       <h1>Home</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, iure? Debitis laborum voluptate modi.
         Fugiat animi sequi placeat id provident in quo fugit molestiae deserunt laudantium assumenda officiis earum magnam,
         architecto veritatis sunt temporibus maxime voluptate? Enim perspiciatis reprehenderit hic quia quam!
         Tenetur nemo debitis dolorum reprehenderit? A vero qui ipsum repellendus delectus!
         Pariatur hic consectetur omnis eos voluptatibus placeat blanditiis at repellat.
-        Repellendus quo amet quae? Aspernatur mollitia facere voluptatem atque perferendis dolorem necessitatibus neque nesciunt officia illo cum vero autem,
-        architecto harum pariatur! Molestiae quia quisquam excepturi facere atque quas perferendis repellendus qui minus.
-        Odit, ipsam. Veritatis est aliquid tempora hic quasi temporibus amet suscipit atque labore ratione expedita earum veniam doloribus praesentium numquam,
-        dolores perferendis. Qui beatae quidem explicabo dolore adipisci cum voluptatem,
-        labore cupiditate, dolor voluptatum ratione natus, perspiciatis vitae a voluptate odio aliquid vel sunt!
-        Maiores adipisci officia eligendi tempore minus dolor omnis fugiat voluptas, consectetur voluptatibus dolorum,
-        rem architecto optio magni quia dignissimos saepe, beatae libero totam voluptate id recusandae earum nobis?
-        Iure voluptate sed molestiae porro, quas asperiores. Consequatur,
-        vel quidem error est quaerat quos voluptatem dolores tempora eaque tenetur nulla dicta dolorem illo fugiat accusamus suscipit,
-        iste sint ullam praesentium dolor facere amet iure delectus deleniti!
-        Dolor iste quia veniam sunt fugit! Maiores, esse harum blanditiis consequatur quo fuga sunt cumque?
-        Modi magnam ipsum sed quam suscipit eaque, temporibus repellendus pariatur id perferendis nulla nemo deserunt odio similique distinctio,
-        ratione, fuga quos. Quo maxime quod ipsam aspernatur laborum, libero eos ullam possimus?</p>
-    </Container>
+       </p>
+      <Button variant="contained" size="large">
+        <RocketLaunchIcon sx={{ mr: 1 }} /> Hire Me
+      </Button>
+      {/* <Button variant="contained" size="large">
+          <HowToRegIcon sx={{ mr: 1 }} /> Hire Me
+        </Button>
+        <Button variant="contained" size="large">
+          <FlightTakeoffIcon sx={{ mr: 1 }} /> Hire Me
+        </Button>
+        <Button variant="contained" size="large">
+          <EmojiPeopleIcon sx={{ mr: 1 }} /> Hire Me
+        </Button>
+        <Button variant="contained" size="large">
+          <CheckCircleIcon sx={{ mr: 1 }} /> Hire Me
+        </Button> */}
+    </div>
   </div>;
+
 }
 
 export default Home;
+
+<Container fixed maxWidth="xl"></Container>
